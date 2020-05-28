24a04b3a5e7afe74d19d289d9fc8ec68cf3f9449
lideng <453321481@qq.com>
2020-05-28 18:09:20 +0800

    打包

--- admin-client_final/src/api/ajax.js
+++ admin-client_final/src/api/ajax.js
@@ -59,3 +59,47 @@ service.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+
+// service.interceptors.response.use(
+ 
+//   response => {
+//     const res = response.data
+
+  
+//     if (res.code !== 200) {
+//       Message({
+//         message: res.message || 'Error',
+//         type: 'error',
+//         duration: 5 * 1000
+//       })
+
+//       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+//       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+//         // to re-login
+//         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
+//           confirmButtonText: 'Re-Login',
+//           cancelButtonText: 'Cancel',
+//           type: 'warning'
+//         }).then(() => {
+//           store.dispatch('user/resetToken').then(() => {
+//             location.reload()
+//           })
+//         })
+//       }
+//       return Promise.reject(new Error(res.message || 'Error'))
+//     } else {
+//       return res
+//     }
+//   },
+//   error => {
+//     console.log('err' + error) // for debug
+//     Message({
+//       message: error.message,
+//       type: 'error',
+//       duration: 5 * 1000
+//     })
+//     return Promise.reject(error)
+//   }
+// )
+
